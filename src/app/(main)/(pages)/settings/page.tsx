4f54554264b832e243c431bc15c8b9dda0584815
LazY <darshitmistry2@gmail.com>
2024-03-29 17:04:13 +0530

    feat : INfo Bar

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        Settings
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+            <h2 className="text-2xl font-bold">User Profile</h2>
+            <p className="text-base text-white/50">
+                Add or Update Your Information
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
